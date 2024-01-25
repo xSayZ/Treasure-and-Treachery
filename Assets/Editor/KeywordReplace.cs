9fe79f9f1b34eb3aefc3bdb1a43604fe25ec9a19
Felix <Sweg1223@gmail.com>
2024-01-25 09:47:38 +0100

    Added Default Script template

---
+++ Assets/Editor/KeywordReplace.cs
@@ -0,0 +1,39 @@
+// /*------------------------------
+// --------------------------------
+// Creation Date: #DATETIME#
+// Author: #DEVELOPER#
+// Description: #PROJECTNAME#
+// --------------------------------
+// ------------------------------*/
+
+using UnityEngine;
+using UnityEditor;
+
+public class KeywordReplace : UnityEditor.AssetModificationProcessor
+{
+    public static void OnWillCreateAsset(string path)
+    {
+        path = path.Replace(".meta", "");
+        int index = path.LastIndexOf(".");
+        if (index < 0)
+            return;
+
+        string file = path.Substring(index);
+        if (file != ".cs" && file != ".js" && file != ".boo")
+            return;
+
+        index = Application.dataPath.LastIndexOf("Assets");
+        path = Application.dataPath.Substring(0, index) + path;
+        if (!System.IO.File.Exists(path))
+            return;
+
+        string fileContent = System.IO.File.ReadAllText(path);
+
+        fileContent = fileContent.Replace("#DATETIME#", System.DateTime.Today.ToString("dd/MM/yy") + "");
+        fileContent = fileContent.Replace("#PROJECTNAME#", PlayerSettings.productName);
+        fileContent = fileContent.Replace("#DEVELOPER#", System.Environment.UserName);
+
+        System.IO.File.WriteAllText(path, fileContent);
+        AssetDatabase.Refresh();
+    }
+}

984592e54867ebd3649ac56b0beeea86d7cfc2c7
Frisbee7846 <Aj.fredrik.johansson@gmail.com>
2024-01-29 12:16:42 +0100

    Created CharacterControllerscript and Movement

---
+++ Assets/Scripts/PlayerController/PlayerMovementBehaviour.cs
@@ -0,0 +1,42 @@
+// /*------------------------------
+// --------------------------------
+// Creation Date: 2024-01-29
+// Author: c21frejo
+// Description: Operation_Donken
+// --------------------------------
+// ------------------------------*/
+
+using UnityEngine;
+
+
+namespace Game {
+    namespace PlayerController
+    {
+        public class PlayerMovementBehaviour : MonoBehaviour
+        {
+            
+
+        #region Unity Functions
+            // Start is called before the first frame update
+            void Start()
+            {
+                
+            }
+    
+            // Update is called once per frame
+            void Update()
+            {
+                
+            }
+        #endregion
+
+        #region Public Functions
+
+#endregion
+
+        #region Private Functions
+
+#endregion
+        }
+    }
+}

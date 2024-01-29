4749f7399e08cdf9d26c4f53cce1bbddb3451b8e
Alex25820 <50142558+Alex25820@users.noreply.github.com>
2024-01-29 14:35:40 +0100

    Created enemy states
    Created enemy states

---
+++ Assets/Scripts/Enemy/EnemyStates/ChaseEnemyState.cs
@@ -0,0 +1,24 @@
+// /*------------------------------
+// --------------------------------
+// Creation Date: 2024-01-29
+// Author: b22alesj
+// Description: Enemy chase state
+// --------------------------------
+// ------------------------------*/
+
+using UnityEngine;
+
+
+namespace Game {
+    namespace Enemy {
+        [System.Serializable]
+        public class ChaseEnemyState : EnemyState
+        {
+#region Public Functions
+            public override void Enter(){}
+            
+            public override void FixedUpdate(){}
+ #endregion
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy/EnemyStates/RoamEnemyState.cs
@@ -0,0 +1,24 @@
+// /*------------------------------
+// --------------------------------
+// Creation Date: 2024-01-29
+// Author: b22alesj
+// Description: Enemy roam state
+// --------------------------------
+// ------------------------------*/
+
+using UnityEngine;
+
+
+namespace Game {
+    namespace Enemy {
+        [System.Serializable]
+        public class RoamEnemyState : EnemyState
+        {
+#region Public Functions
+            public override void Enter(){}
+            
+            public override void FixedUpdate(){}
+ #endregion
+        }
+    }
+}

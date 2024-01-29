fc3c7a986b520228b4d7a978ca49bf1fe1fef413
Felix <Sweg1223@gmail.com>
2024-01-29 13:14:51 +0100

    Player Spawning

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -15,6 +15,14 @@ namespace Game {
     namespace Backend {
         public class GameManager : MonoBehaviour
         {
+
+            public GameObject playerPrefab;
+            public int numberOfPlayers;
+
+            public Transform spawnRingCenter;
+            [Range(0.5f, 5f)]
+            public float spawnRingRadius;
+
             [SerializeField] private List<PlayerController> activePlayerControllers;
 
 
@@ -22,13 +30,22 @@ namespace Game {
             #region Unity Functions
             void Awake()
             {
-                players = new List<PlayerController>();
             }
             void Start()
             {
-                
+                activePlayerControllers = new List<PlayerController>();
+
+                for (int i = 0; i < numberOfPlayers; i++)
+                {
+                    Vector3 _spawnPosition = CalculatePositionInRing(i, numberOfPlayers);
+                    Quaternion _spawnRotation = Quaternion.identity;
+
+                    // TODO: Add spawnPosition and spawnRotation
+                    GameObject _spawnedPlayer = Instantiate(playerPrefab, _spawnPosition, _spawnRotation) as GameObject;
+                    AddPlayersToActiveList(_spawnedPlayer.GetComponent<PlayerController>());
+                }
             }
-    
+
             // Update is called once per frame
             void Update()
             {
@@ -42,7 +59,23 @@ namespace Game {
 
 #region Private Functions
 
-#endregion
+
+            private void AddPlayersToActiveList(PlayerController newPlayer)
+            {
+                activePlayerControllers.Add(newPlayer);
+            }
+
+            Vector3 CalculatePositionInRing(int positionID, int numberOfPlayers)
+            {
+                if (numberOfPlayers == 1)
+                    return spawnRingCenter.position;
+
+                float angle = (positionID) * Mathf.PI * 2 / numberOfPlayers;
+                float x = Mathf.Cos(angle) * spawnRingRadius;
+                float z = Mathf.Sin(angle) * spawnRingRadius;
+                return spawnRingCenter.position + new Vector3(x, 0, z);
+            }
+            #endregion
         }
     }
 }

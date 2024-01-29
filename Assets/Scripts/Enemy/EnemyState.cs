d292b392a0dbaf789d49288beafcb13df0ed0771
Alex25820 <50142558+Alex25820@users.noreply.github.com>
2024-01-29 17:20:25 +0100

    Added listen for target function
    Added listen for target function

--- Assets/Scripts/Enemy/EnemyState.cs
+++ Assets/Scripts/Enemy/EnemyState.cs
@@ -29,10 +29,24 @@ namespace Game {
 
 
 #region Protcted Functions
-            protected List<Vector3> LookForTarget(Vector3 lookPosition, Vector3 lookDirection, float lookDistance, float lookFov, LayerMask playerLayerMask, LayerMask obstacleLayerMask)
+
+            protected List<Vector3> ListenForTarget(Vector3 listenPosition, float listenRange, LayerMask targetLayerMask)
+            {
+                List<Vector3> audibleTargetPositions = new List<Vector3>();
+                Collider[] targetsInHearingRadius = Physics.OverlapSphere(listenPosition, listenRange, targetLayerMask);
+                
+                for (int i = 0; i < targetsInHearingRadius.Length; i++)
+                {
+                    audibleTargetPositions.Add(targetsInHearingRadius[i].transform.position);
+                }
+
+                return audibleTargetPositions;
+            }
+
+            protected List<Vector3> LookForTarget(Vector3 lookPosition, Vector3 lookDirection, float lookRange, float lookFov, LayerMask targetLayerMask, LayerMask obstacleLayerMask)
             {
                 List<Vector3> visibleTargetPositions = new List<Vector3>();
-                Collider[] targetsInViewRadius = Physics.OverlapSphere(lookPosition, lookDistance, playerLayerMask);
+                Collider[] targetsInViewRadius = Physics.OverlapSphere(lookPosition, lookRange, targetLayerMask);
                 
                 for (int i = 0; i < targetsInViewRadius.Length; i++)
                 {

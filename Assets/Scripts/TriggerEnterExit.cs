a6f55f5fe6396b884ede6fc7a00f4b7f85db9677
Alex25820 <50142558+Alex25820@users.noreply.github.com>
2024-01-30 10:20:14 +0100

    Created script for sending unity events when a trigger is entered or exited
    Created script for sending unity events when a trigger is entered or exited

---
+++ Assets/Scripts/TriggerEnterExit.cs
@@ -0,0 +1,31 @@
+// /*------------------------------
+// --------------------------------
+// Creation Date: 2024-01-30
+// Author: alexa
+// Description: Triggers a unity event when an object enters or exits a collider
+// --------------------------------
+// ------------------------------*/
+
+using UnityEngine;
+using UnityEngine.Events;
+
+
+namespace Game {
+    namespace Other {
+        public class TriggerEnterExit : MonoBehaviour
+        {
+            [SerializeField] private UnityEvent<Transform> triggerEnter;
+            [SerializeField] private UnityEvent<Transform> triggerExit;
+            
+            private void OnTriggerEnter(Collider other)
+            {
+                triggerEnter.Invoke(other.transform);
+            }
+            
+            private void OnTriggerExit(Collider other)
+            {
+                triggerExit.Invoke(other.transform);
+            }
+        }
+    }
+}

e1eafa9ab8604befb139f27a64608dca4e01219b
Alex25820 <50142558+Alex25820@users.noreply.github.com>
2024-01-30 12:02:44 +0100

    Created alert enemy state
    Created alert enemy state

--- Assets/Scripts/Enemy/EnemyStates/AlertEnemyState.cs
+++ Assets/Scripts/Enemy/EnemyStates/AlertEnemyState.cs
@@ -14,10 +14,50 @@ namespace Game {
         [System.Serializable]
         public class AlertEnemyState : EnemyState
         {
-#region State Machine Functions
-            //public override void Enter(){}
+            [SerializeField] private float lookTime;
+            [SerializeField] private float rotationSpeed;
             
-            //public override void FixedUpdate(){}
+            private float currentLookTime;
+            private Vector3 lastHeardPosition;
+            
+#region State Machine Functions
+            public override void Enter()
+            {
+                Debug.Log("Alert");
+                
+                currentLookTime = 0;
+                if (enemyController.targetsInHearingRange.Count > 0)
+                {
+                    lastHeardPosition = GetClosestTarget(enemyController.targetsInHearingRange).position;
+                }
+            }
+
+            public override void FixedUpdate()
+            {
+                // Check if seen target
+                if (enemyController.targetsInVisionRange.Count > 0)
+                {
+                    enemyController.ChangeState(enemyController.GrowlEnemyState);
+                }
+                
+                // Look cooldown
+                currentLookTime += Time.fixedDeltaTime;
+                if (currentLookTime >= lookTime)
+                {
+                    enemyController.ChangeState(enemyController.RoamEnemyState);
+                }
+                
+                // Update last heard position
+                if (enemyController.targetsInHearingRange.Count > 0)
+                {
+                    lastHeardPosition = GetClosestTarget(enemyController.targetsInHearingRange).position;
+                }
+                
+                // Turn towards last heard position
+                Vector3 _targetDirection = (lastHeardPosition - enemyController.transform.position).normalized;
+                Quaternion _lookRotation = Quaternion.LookRotation(_targetDirection);
+                enemyController.transform.rotation = Quaternion.Slerp(enemyController.transform.rotation, _lookRotation, Time.fixedDeltaTime * rotationSpeed);
+            }
             
             //public override void Exit(){}
  #endregion

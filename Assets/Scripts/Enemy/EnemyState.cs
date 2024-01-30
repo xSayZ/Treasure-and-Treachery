5036bcc4298100677f6fb7f5ac983092df3cbf2f
Alex25820 <50142558+Alex25820@users.noreply.github.com>
2024-01-30 12:02:21 +0100

    Added get closest target function to enemy state
    Added get closest target function to enemy state

--- Assets/Scripts/Enemy/EnemyState.cs
+++ Assets/Scripts/Enemy/EnemyState.cs
@@ -6,6 +6,7 @@
 // --------------------------------
 // ------------------------------*/
 
+using System.Collections.Generic;
 using UnityEngine;
 
 
@@ -26,6 +27,27 @@ namespace Game {
             public virtual void FixedUpdate(){}
             
             public virtual void Exit(){}
+#endregion
+
+#region Protected Functions
+            protected Transform GetClosestTarget(List<Transform> _targets)
+            {
+                Transform _closestTarget = _targets[0];
+                float _closestDistance = Vector3.Distance(enemyController.transform.position, _targets[0].position);
+                             
+                for (int i = 1; i < _targets.Count; i++)
+                {
+                    float _distance = Vector3.Distance(enemyController.transform.position, _targets[i].position);
+                                 
+                    if (_distance < _closestDistance)
+                    {
+                        _closestTarget = _targets[i];
+                        _closestDistance = _distance;
+                    }
+                }
+                             
+                return _closestTarget;
+            }
 #endregion
         }
     }

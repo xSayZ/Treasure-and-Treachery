26c0f0c65bced90527620970828e628162dca80b
Alex25820 <50142558+Alex25820@users.noreply.github.com>
2024-01-30 12:03:31 +0100

    Created chase enemy state
    Created chase enemy state

--- Assets/Scripts/Enemy/EnemyStates/ChaseEnemyState.cs
+++ Assets/Scripts/Enemy/EnemyStates/ChaseEnemyState.cs
@@ -7,6 +7,7 @@
 // ------------------------------*/
 
 using UnityEngine;
+using UnityEngine.AI;
 
 
 namespace Game {
@@ -14,10 +15,45 @@ namespace Game {
         [System.Serializable]
         public class ChaseEnemyState : EnemyState
         {
-#region State Machine Functions
-            //public override void Enter(){}
+            [SerializeField] private float maxDeviationAngle;
             
-            //public override void FixedUpdate(){}
+            private NavMeshAgent navMeshAgent;
+            private Vector3 lastSeenPosition;
+            
+#region State Machine Functions
+            public override void Enter()
+            {
+                Debug.Log("Chase");
+                
+                navMeshAgent = enemyController.GetNavMeshAgent();
+                
+                lastSeenPosition = GetClosestTarget(enemyController.targetsInVisionRange).position;
+                navMeshAgent.destination = lastSeenPosition;
+            }
+
+            public override void FixedUpdate()
+            {
+                // Update last seen position
+                if (enemyController.targetsInVisionRange.Count > 0)
+                {
+                    lastSeenPosition = GetClosestTarget(enemyController.targetsInVisionRange).position;
+                }
+                
+                // Update nav mesh agent destination if necessary
+                Vector3 _directionToTarget = (navMeshAgent.destination - enemyController.transform.position).normalized;
+                Vector3 _directionToLastSeenPosition = (lastSeenPosition - enemyController.transform.position).normalized;
+                if (Vector3.Angle(_directionToTarget, _directionToLastSeenPosition) > maxDeviationAngle)
+                {
+                    navMeshAgent.destination = lastSeenPosition;
+                    Debug.Log("Updated nav mesh agent destination");
+                }
+                
+                // Lost target
+                if (Vector3.Distance(enemyController.transform.position, lastSeenPosition) <= navMeshAgent.stoppingDistance)
+                {
+                    enemyController.ChangeState(enemyController.AlertEnemyState);
+                }
+            }
             
             //public override void Exit(){}
  #endregion

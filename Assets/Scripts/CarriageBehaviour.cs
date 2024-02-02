05f7541d930c0322666537a1b43bdeacc2a94a96
Alex25820 <50142558+Alex25820@users.noreply.github.com>
2024-02-02 15:56:56 +0100

    Updated carriage
    Updated carriage

--- Assets/Scripts/CarriageBehaviour.cs
+++ Assets/Scripts/CarriageBehaviour.cs
@@ -2,72 +2,63 @@
 // --------------------------------
 // Creation Date: 2024-01-29
 // Author: b22feldy
-// Description: Operation_Donken
+// Description: Carriage for leaving level
 // --------------------------------
 // ------------------------------*/
 
+using Game.Backend;
 using UnityEngine;
-using Game.Events;
+using Game.Quest;
 using Game.Player;
 
 namespace Game {
     namespace Scenes {
         public class CarriageBehaviour : MonoBehaviour
         {
-            private bool canDoObjective = false;
-            private bool availableToEndGame = false;
-            [SerializeField] private int amountForObjective = 3;
-            private int playerID;
-            private int playerCounter;
-            [SerializeField] GameObject carriageTeleport;
+            [SerializeField] GameObject playerTeleportPosition;
             
-            private void Start() {
-                EventManager.OnObjectivePickup.AddListener(ObjectiveCheck);
-            }
+            private bool canLeave;
+            private int playersInCarriage;
 
 #region Unity Functions
-            void OnTriggerEnter(Collider other)
+            private void OnEnable()
             {
-                if (other.CompareTag("Player") && 
-                    canDoObjective && 
-                    other.gameObject.transform.GetComponent<PlayerController>().PlayerData.playerIndex == playerID)
+                QuestManager.OnAllRequiredQuestsCompleted.AddListener(AllRequiredQuestsCompleted);
+            }
+            
+            private void OnDisable()
+            {
+                QuestManager.OnAllRequiredQuestsCompleted.RemoveListener(AllRequiredQuestsCompleted);
+            }
+            
+            private void OnTriggerEnter(Collider other)
+            {
+                if (other.CompareTag("Player"))
                 {
-                    //TODO: Send to player that they can use pickup
-                    // When they use the shovel, decrease amountForObjective
-                    // If amountForObjective is 0, stop them from being able to use shovel then make it available to leave
-                    if (amountForObjective == 0) {
-                        canDoObjective = false;
-                        availableToEndGame = true;
+                    if (canLeave)
+                    {
+                        GameObject player = other.gameObject;
+                        player.GetComponent<PlayerController>().SetInputActiveState(true);
+                        player.transform.position = playerTeleportPosition.transform.position;
+                        player.transform.localScale = new Vector3(0,0,0);
+
+                        playersInCarriage++;
+                        if (playersInCarriage >= GameManager.Instance.activePlayerControllers.Count)
+                        {
+                            // All players are in carriage, time to end level
+                            Debug.Log("Level Done");
+                        }
                     }
                 }
-
-                if (other.CompareTag("Player") && availableToEndGame) {
-                    var player = other.gameObject;
-                    player.GetComponent<PlayerController>().SetInputActiveState(availableToEndGame);
-                    player.transform.position = carriageTeleport.transform.position;
-                    player.transform.localScale = new Vector3(0,0,0);
-                }
             }
-
-
-#endregion
-
-#region Public Functions
-
-            public void UpdateObjective() {
-                amountForObjective--;
-            }
-
 #endregion
 
 #region Private Functions
-
-            private void ObjectiveCheck(bool arg0, int playerIndex){
-                canDoObjective = arg0;
-                playerID = playerIndex;
+            private void AllRequiredQuestsCompleted()
+            {
+                canLeave = true;
             }
 #endregion
-
         }
     }
 }

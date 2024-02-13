8140e9a61d8d93f30300810145ce95574a1f69d8
Felix <Sweg1223@gmail.com>
2024-02-13 09:29:47 +0100

    Player UI

---
+++ Assets/Scripts/Player/Sub-Behaviours/PlayerUIDisplayBehaviours.cs
@@ -0,0 +1,52 @@
+// /*------------------------------
+// --------------------------------
+// Creation Date: 2024-02-13
+// Author: b22feldy
+// Description: Operation_Donken
+// --------------------------------
+// ------------------------------*/
+
+using Game.Backend;
+using Game.Enemy;
+using Game.Quest;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+
+namespace Game {
+    namespace Player {
+        public class PlayerUIDisplayBehaviours : MonoBehaviour {
+            
+            public TextMeshProUGUI currencyText, killsText;
+            public GameObject playerUIElements;
+
+            private int kills;
+
+            public void SetupBehaviour() {
+                playerUIElements.SetActive(false);
+                
+                QuestManager.OnGoldPickedUp.AddListener(UpdateCurrencyDisplay);
+                EnemyManager.OnEnemyDeath.AddListener(UpdateKillsDisplay);
+                
+                
+                currencyText.SetText("0");
+                killsText.SetText("0");
+                
+            }
+
+            private void UpdateCurrencyDisplay(int _playerID, int _currency) {
+                currencyText.SetText((_currency).ToString());
+            }
+
+            private void UpdateKillsDisplay(EnemyController _enemy) {
+                kills++;
+                currencyText.SetText((kills).ToString());
+            }
+
+            public void TogglePlayerUIElements(bool _active) {
+                playerUIElements.SetActive(_active);
+            }
+        }
+    }
+}

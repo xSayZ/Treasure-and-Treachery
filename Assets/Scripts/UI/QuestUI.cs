d406a614d8e0161f6eac7d8b57060e14168f05cc
Alex25820 <50142558+Alex25820@users.noreply.github.com>
2024-02-13 09:59:36 +0100

    Created quest UI
    Created quest UI

--- Assets/Scripts/UI/QuestUI.cs
+++ Assets/Scripts/UI/QuestUI.cs
@@ -6,6 +6,10 @@
 // --------------------------------
 // ------------------------------*/
 
+using System.Collections.Generic;
+using Game.Core;
+using Game.Quest;
+using Game.Scene;
 using TMPro;
 using UnityEngine;
 
@@ -16,14 +20,35 @@ namespace Game {
         {
             [Header("Setup")]
             [SerializeField] private TextMeshProUGUI questText;
-
-            [Header("Settings")]
+            
+            [Header("Start")]
             [SerializeField] private string startingText;
             [SerializeField] private float startingShowTime;
+            
+            [Header("Events")]
+            [SerializeField] private List<QuestUIItem> QuestUIItems;
+            
+            [System.Serializable]
+            private class QuestUIItem
+            {
+                public Pickup Pickup;
+                public string Text;
+                public float ShowingTime;
+            }
             
             private float showTimeLeft;
             
 #region Unity Functions
+            private void OnEnable()
+            {
+                QuestManager.OnItemPickedUp.AddListener(ItemPickedUp);
+            }
+            
+            private void OnDisable()
+            {
+                QuestManager.OnItemPickedUp.RemoveListener(ItemPickedUp);
+            }
+            
             private void Start()
             {
                 SetText(startingText, startingShowTime);
@@ -42,12 +67,24 @@ namespace Game {
             }
 #endregion
 
-#region Public Functions
-            public void SetText(string _text, float _showTime)
+#region Private Functions
+            private void SetText(string _text, float _showTime)
             {
                 questText.text = _text;
                 showTimeLeft = _showTime;
                 questText.gameObject.SetActive(true);
+            }
+            
+            private void ItemPickedUp(int _playerIndex, Item _item)
+            {
+                for (int i = QuestUIItems.Count - 1; i >= 0; i--)
+                {
+                    if (QuestUIItems[i].Pickup.GetItem() == _item)
+                    {
+                        SetText(QuestUIItems[i].Text, QuestUIItems[i].ShowingTime);
+                        QuestUIItems.Remove(QuestUIItems[i]);
+                    }
+                }
             }
 #endregion
         }

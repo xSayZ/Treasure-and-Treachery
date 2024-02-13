650452a127e29305fd1cd2ad86e723531c504068
Felix <Sweg1223@gmail.com>
2024-02-13 16:41:14 +0100

    Enemy Ghost with Animations

---
+++ Assets/Scripts/Enemy/EnemyAnimationBehaviour.cs
@@ -0,0 +1,47 @@
+// /*------------------------------
+// --------------------------------
+// Creation Date: 2024-02-13
+// Author: b22feldy
+// Description: Operation_Donken
+// --------------------------------
+// ------------------------------*/
+
+using System;
+using UnityEngine;
+
+
+namespace Game {
+    namespace NAME {
+        public class EnemyAnimationBehaviour : MonoBehaviour
+        {
+            [Header("Component References")]
+            public Animator EnemyAnimator;
+
+            private int enemyMovementAnimationID;
+            private int enemyAttackAnimationID;
+
+            public void SetupBehaviour()
+            {
+                EnemyAnimator = GetComponent<Animator>();
+                SetupAnimationIDs();
+            }
+
+            void SetupAnimationIDs()
+            {
+                enemyMovementAnimationID = Animator.StringToHash("Movement");
+                enemyAttackAnimationID = Animator.StringToHash("Attack");
+            }
+
+            public void UpdateMovementAnimation(float movementBlendValue)
+            {
+                Debug.Log(movementBlendValue);
+                EnemyAnimator.SetFloat(enemyMovementAnimationID, movementBlendValue);
+            }
+
+            public void PlayAttackAnimation()
+            {
+                EnemyAnimator.SetTrigger(enemyAttackAnimationID);
+            }
+        }
+    }
+}

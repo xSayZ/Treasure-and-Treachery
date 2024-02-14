808dc2f7507ba4c2643bb8031709be212d1d2aac
Frisbee7846 <Aj.fredrik.johansson@gmail.com>
2024-02-14 10:42:01 +0100

    small tweaks

--- Assets/Shaders/CutoutObject.cs
+++ Assets/Shaders/CutoutObject.cs
@@ -76,8 +76,8 @@ namespace Game
 
             private void OnDrawGizmos(){
             
-                Gizmos.color = Color.black;
-                Gizmos.DrawRay(cam.transform.position,target.transform.position-cam.transform.position);
+                //Gizmos.color = Color.black;
+                //Gizmos.DrawRay(cam.transform.position,target.transform.position-cam.transform.position);
                 
                 
             }

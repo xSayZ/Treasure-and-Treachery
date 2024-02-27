f9dbfab168363cb4870cdba16bb37b9a9114d6f3
Alex25820 <50142558+Alex25820@users.noreply.github.com>
2024-02-27 09:39:49 +0100

    Witch now takes damage when using wave
    Witch now takes damage when using wave

--- Assets/Scripts/Player/Sub-Behaviours/PlayerAttackBehaviour.cs
+++ Assets/Scripts/Player/Sub-Behaviours/PlayerAttackBehaviour.cs
@@ -50,6 +50,7 @@ namespace Game {
             [SerializeField] private float rangedKnockbackSpeed;
             [SerializeField] private float rangedKnockbackTime;
             [SerializeField] private Transform projectileSpawnPoint;
+            [SerializeField] private int rangedWaveHealthCost;
             
             [Header("Audio")]
             [SerializeField] private GameObject playerObj;
@@ -309,7 +310,7 @@ namespace Game {
                 }
                 else
                 {
-                    if (currentAimAngle < rangedAimMaxAngle)
+                    if (currentAimAngle < rangedAimMaxAngle || playerController.Health <= rangedWaveHealthCost)
                     {
                         GameObject _projectile = Instantiate(normalProjectile, projectileSpawnPoint.position, Quaternion.LookRotation(Quaternion.identity * transform.forward));
                         _projectile.GetComponent<Projectile>().Setup(rangedAttackDamage, playerController.PlayerData, OnKill);
@@ -318,6 +319,7 @@ namespace Game {
                     {
                         GameObject _projectile = Instantiate(waveProjectile, projectileSpawnPoint.position, Quaternion.LookRotation(Quaternion.identity * transform.forward));
                         _projectile.GetComponent<WaveProjectile>().Setup(rangedAttackDamage, playerController.PlayerData, OnWaveKill);
+                        (playerController as IDamageable).Damage(rangedWaveHealthCost);
                     }
                 }
             }

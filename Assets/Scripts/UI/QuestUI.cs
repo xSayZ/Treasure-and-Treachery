56ce46e36b4815ee6aa18b93fad2ac6049484d5a
Alex25820 <50142558+Alex25820@users.noreply.github.com>
2024-02-28 16:17:56 +0100

    Updated quest canvas with animations
    Updated quest canvas with animations

--- Assets/Scripts/UI/QuestUI.cs
+++ Assets/Scripts/UI/QuestUI.cs
@@ -16,6 +16,8 @@ namespace Game {
         {
             [Header("Setup")]
             [SerializeField] private TextMeshProUGUI questText;
+            [SerializeField] private Animator spriteAnimator;
+            [SerializeField] private Animator maskAnimator;
             
             [Header("Settings")]
             [SerializeField] private float showTime;
@@ -26,14 +28,17 @@ namespace Game {
 #region Unity Functions
             private void Start()
             {
-                DisplayText(startingText);
+                questText.text = startingText;
+                showTimeLeft = showTime;
+                questText.gameObject.SetActive(true);
             }
 
             private void Update()
             {
                 if (showTimeLeft <= 0)
                 {
-                    questText.gameObject.SetActive(false);
+                    spriteAnimator.SetTrigger("Close");
+                    maskAnimator.SetTrigger("Close");
                 }
                 else
                 {
@@ -45,9 +50,7 @@ namespace Game {
 #region Public Functions
             public void DisplayText(string _text)
             {
-                questText.text = _text;
-                showTimeLeft = showTime;
-                questText.gameObject.SetActive(true);
+                
             }
 #endregion
         }

df32566202314e953dc61fa51f1f8a325f55ffd1
Alex25820 <50142558+Alex25820@users.noreply.github.com>
2024-02-28 10:12:01 +0100

    Created script to lock transform
    Created script to lock transform

---
+++ Assets/Scripts/Utility/TransformLock.cs
@@ -0,0 +1,57 @@
+// /*------------------------------
+// --------------------------------
+// Creation Date: 2024-02-28
+// Author: b22alesj
+// Description: Operation_Donken
+// --------------------------------
+// ------------------------------*/
+
+using UnityEngine;
+
+
+namespace Utility {
+    namespace EditorHelper {
+        [ExecuteAlways]
+        public class TransformLock : MonoBehaviour
+        {
+            [Header("Position")]
+            [SerializeField] private bool lockPosition;
+            [SerializeField] private Vector3 position;
+            
+            [Header("Rotation")]
+            [SerializeField] private bool lockRotation;
+            [SerializeField] private Vector3 rotation;
+            
+            [Header("Scale")]
+            [SerializeField] private bool lockScale;
+            [SerializeField] private Vector3 scale = new Vector3(1, 1, 1);
+
+            #if (UNITY_EDITOR)
+            private void Update()
+            {
+                if (transform.hasChanged)
+                {
+                    transform.hasChanged = false;
+
+                    if (lockPosition)
+                    {
+                        transform.localPosition = position;
+                    }
+                    
+                    if (lockRotation)
+                    {
+                        transform.localRotation = Quaternion.Euler(rotation);
+                    }
+                    
+                    if (lockScale)
+                    {
+                        transform.localScale = scale;
+                    }
+                    
+                    print("The transform has changed!");
+                }
+            }
+            #endif
+        }
+    }
+}

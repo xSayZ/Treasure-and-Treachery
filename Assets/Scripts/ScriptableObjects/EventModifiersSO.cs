8b1ff639dd422eeeea0a4dcd1f16fec7d274e43c
Felix <Sweg1223@gmail.com>
2024-03-14 12:00:03 +0100

    a

---
+++ Assets/Scripts/ScriptableObjects/EventModifiersSO.cs
@@ -0,0 +1,25 @@
+// /*------------------------------
+// --------------------------------
+// Creation Date: 2024-03-14
+// Author: b22feldy
+// Description: Treasure and Treachery
+// --------------------------------
+// ------------------------------*/
+
+using UnityEngine;
+
+
+namespace Game {
+    namespace ScriptableObjects {
+        public class EventModifiersSO : MonoBehaviour
+        {
+            private float speedModifier;
+            private float damageModifier;
+            private float healthModifier;
+            private float timeModifier;
+            private float objectiveScoreModifier;
+            
+            
+        }
+    }
+}

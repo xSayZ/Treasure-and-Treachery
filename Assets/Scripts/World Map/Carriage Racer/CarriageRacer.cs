7b28badcaa3953819f5c27a2a5434e39e470b581
Alex25820 <50142558+Alex25820@users.noreply.github.com>
2024-03-14 16:45:21 +0100

    Carriage racer now works correctly with both controller and keyboard
    Carriage racer now works correctly with both controller and keyboard

--- Assets/Scripts/World Map/Carriage Racer/CarriageRacer.cs
+++ Assets/Scripts/World Map/Carriage Racer/CarriageRacer.cs
@@ -7,37 +7,60 @@
 // ------------------------------*/
 
 using System.Collections.Generic;
+using Game.Backend;
+using Game.CharacterSelection;
 using Game.Managers;
 using UnityEngine;
 using UnityEngine.InputSystem;
+
 
 namespace Game {
     namespace Racer {
         public class CarriageRacer : MonoBehaviour
         {
+            [Header("Setup")]
+            [SerializeField] private GameObject racerPlayerInputPrefab;
+            
             [Header("Sub Behaviours")]
             [SerializeField] private CarriageMovementBehaviour carriageMovementBehaviour;
             [SerializeField] private CarriageAnimationBehaviour carriageAnimationBehaviour;
             
-            [Header("Player Inputs")]
+            [Header("Debug")]
             [SerializeField] private List<Vector2> activeLeftStickValues = new List<Vector2>();
             
             private Vector3 averageLeftStickValue;
 
 #region Unity Functions
-            private void Start() {
+            private void Awake()
+            {
+                if (CharacterSelect.selectedCharacters.Count == 0)
+                {
+                    foreach (InputDevice _inputDevice in InputSystem.devices)
+                    {
+                        Instantiate(racerPlayerInputPrefab).GetComponent<RacerPlayerInput>().Setup(this, _inputDevice);
+                        activeLeftStickValues.Add(new Vector2());
+                    }
+                }
+                else
+                {
+                    foreach (KeyValuePair<InputDevice, PlayerData> _kvp in CharacterSelect.selectedCharacters)
+                    {
+                        Instantiate(racerPlayerInputPrefab).GetComponent<RacerPlayerInput>().Setup(this, _kvp.Key);
+                        activeLeftStickValues.Add(new Vector2());
+                    }
+                }
+            }
+
+            private void Start()
+            {
                 transform.position = LevelManager.Instance.worldMapManager.carriagePosition;
                 transform.rotation = LevelManager.Instance.worldMapManager.carriageRotation;
                 carriageMovementBehaviour.SetupBehaviour();
                 carriageAnimationBehaviour.SetupBehaviour();
             }
 
-            private void Update()
+            private void FixedUpdate()
             {
-                OnMovement();
-            }
-
-            private void FixedUpdate() {
                 UpdateCarriageMovement();
             }
 #endregion
@@ -47,33 +70,21 @@ namespace Game {
             {
                 carriageMovementBehaviour.enabled = _active;
             }
-#endregion
 
-#region Private Functions
-            private void UpdateCarriageMovement() {
-                carriageMovementBehaviour.UpdateMovementData(averageLeftStickValue);
-            }
-
-            private void OnMovement() {
-                // Clear the list of active left stick values
-                activeLeftStickValues.Clear();
-                
-                // Iterate through all gamepads
-                foreach (Gamepad gamepad in Gamepad.all) {
-                    // Check if the gamepad is actuated
-                    if (gamepad.IsActuated()) {
-                        
-                        // Get the input from the left stick
-                        var input = gamepad.leftStick.ReadValue();
-                        
-                        // Add the input to the list
-                        activeLeftStickValues.Add(input);
-                    }
-                }
+            public void OnMoveInput(int _playerInputIndex, Vector2 _moveValue)
+            {
+                activeLeftStickValues[_playerInputIndex] = _moveValue;
                 
                 // Calculate the average left stick value
                 averageLeftStickValue = CalculateAverageLeftStickValue();
                 carriageAnimationBehaviour.UpdateMovementAnimation(averageLeftStickValue.magnitude);
+            }
+#endregion
+
+#region Private Functions
+            private void UpdateCarriageMovement()
+            {
+                carriageMovementBehaviour.UpdateMovementData(averageLeftStickValue);
             }
 
             private Vector3 CalculateAverageLeftStickValue()
@@ -82,7 +93,7 @@ namespace Game {
                     return Vector2.zero;
                 
                 Vector2 average = Vector2.zero;
-
+                
                 int notZeroCount = 0;
                 
                 foreach (var value in activeLeftStickValues)
@@ -93,7 +104,7 @@ namespace Game {
                         notZeroCount++;
                     }
                 }
-
+                
                 if (notZeroCount > 0)
                 {
                     average /= notZeroCount;
---
+++ Assets/Scripts/World Map/Carriage Racer/RacerPlayerInput.cs
@@ -0,0 +1,34 @@
+// /*------------------------------
+// --------------------------------
+// Creation Date: 2024-03-14
+// Author: b22alesj
+// Description: Handles individual player inputs for racer
+// --------------------------------
+// ------------------------------*/
+
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+namespace Game {
+    namespace Racer {
+        public class RacerPlayerInput : MonoBehaviour
+        {
+            [Header("Setup")]
+            [SerializeField] private PlayerInput playerInput;
+            
+            private CarriageRacer carriageRacer;
+
+            public void Setup(CarriageRacer _carriageRacer, InputDevice _inputDevice)
+            {
+                carriageRacer = _carriageRacer;
+                playerInput.SwitchCurrentControlScheme(_inputDevice);
+            }
+
+            public void OnMovement(InputAction.CallbackContext _context)
+            {
+                carriageRacer.OnMoveInput(playerInput.user.index, _context.ReadValue<Vector2>());
+            }
+        }
+    }
+}

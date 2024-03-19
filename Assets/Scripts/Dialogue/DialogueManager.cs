f9037f2c57f4ad144bf0aed1b0f4209ad0792a45
a22hercr <a22hercr@student.his.se>
2024-03-19 10:08:09 +0100

    fix

--- Assets/Scripts/Dialogue/DialogueManager.cs
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -6,6 +6,7 @@
 // --------------------------------
 // ------------------------------*/
 
+using System;
 using System.Collections;
 using UnityEngine;
 using Ink.Runtime;
@@ -157,7 +158,15 @@ namespace Game {
                 
                 story.BindExternalFunction("PlayEventAudio", (int eventIndex) => {
                     // Play Sound
-                    dialogueAudio.BossEventDialogue(eventIndex);
+                    try
+                    {
+                        dialogueAudio.BossEventDialogue(eventIndex);
+                    }
+                    catch (Exception e)
+                    {
+                        Debug.LogError("[{DialogueManager}]: Error Exception " + e);
+                    }
+                    
                 });
                 
   #endregion

fbeefad7b8ecfc331ee884c7117c82e7b9b59f4f
Felix <Sweg1223@gmail.com>
2024-03-19 16:26:17 +0100

    Update TimeSetParam.cs

--- Assets/Audio/Music/TimeSetParam.cs
+++ Assets/Audio/Music/TimeSetParam.cs
@@ -40,16 +40,18 @@ namespace Game {
         private void SetParamByTime()
         {
             timer = GameManager.Instance.timer.GetCurrentTime();
+            var _roundTime = GameManager.Instance.roundTime;
                 
             // Set the parameter of the music event based on the timer
-            int[] _thresholds = { 60, 120, 180, 240 };
+            int[] _thresholds = { (int)(_roundTime / 2.5), (int)(_roundTime / 2), (int)(_roundTime / 1.5), (int)(_roundTime / 1.2)};
             float[] _musicParameters = { 2f, 3f, 4f, 5f };
-
+            
             // Set the parameter of the music event based on the timer
             for (int i = 0; i < _thresholds.Length; i++)
             {
                 if (timer >= _thresholds[i])
                 {
+                    Debug.Log("Setting parameter to: " + _musicParameters[i]);
                     // Set the parameter of the music event
                     AudioMananger.Instance.SetParameterMusicEvent(EventsToBePlayed.GamePlayMusic, "MusicProg", _musicParameters[i], false, false);
                 }

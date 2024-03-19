9499904e6c51148fdab45498254657febca788c1
Felix <Sweg1223@gmail.com>
2024-03-19 10:51:06 +0100

    Main Menu

---
+++ Assets/Credits.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credits : MonoBehaviour
+{
+    public void ReturnToMainMenu()
+    {
+        
+    }
+}

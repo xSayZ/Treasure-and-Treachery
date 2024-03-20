3ca4240d77932eb213e26164afb82041f1373c3d
Felix <Sweg1223@gmail.com>
2024-03-20 09:49:59 +0100

    a

--- Assets/Audio/Music/AudioZoneSettings.cs
+++ Assets/Audio/Music/AudioZoneSettings.cs
@@ -38,15 +38,10 @@ namespace Game {
             //skapar arrays av variabeln som innehåller "AudioZSettings" 
             public AudioSettings [] audioZoneSettings;
             
-        #region Unity Functions
-            // Start is called before the first frame update
-
+#region Unity Functions
             private void Start()
             {
                 musicBus = RuntimeManager.GetBus("Bus:/PreMaster/Music Bus");
-                /*musicBus.getPaused(out bool state);
-                Debug.Log("Music Bus state is" + state); */
-                
                 RunAudioSettings();
             }
 
@@ -54,18 +49,16 @@ namespace Game {
             {
                 if (musicCrossOver==true)
                 {
-                    Debug.Log("MusicContinued into next scene");
                     return;
                 }
+                
                 musicBus.stopAllEvents(STOP_MODE.ALLOWFADEOUT);
-                Debug.Log("Unity Scene changed-Music Bus stopped");
-                /*musicBus.getPaused(out bool state);
-                Debug.Log("Music Bus state is" + state);
-                */
-              
             }
 
-            public void RunAudioSettings()
+#endregion
+
+#region Private Functions
+            private void RunAudioSettings()
             {
                 foreach (AudioSettings i in audioZoneSettings)
                 {
@@ -78,39 +71,21 @@ namespace Game {
                     {
                         switch (i.action)
                         {
-                            case Action.None: 
-                                Debug.Log("Action set to None in MusicZoneSettings");
-                                break;
-                  
                             case Action.PlayMusic: 
-                                AudioMananger.Instance.PlayMusicEvent(i.eventsToBePlayed);
-                  
+                                AudioMananger.Instance.PlayMusicEvent(i.eventsToBePlayed); 
                                 break;
-                    
                             case Action.StopMusic: 
                                 AudioMananger.Instance.StopMusicEvent(i.eventsToBePlayed, i.ignoreFadeOut);
-                    
                                 break;
-                  
                             case Action.SetMusicParam:
                                 AudioMananger.Instance.SetParameterMusicEvent(i.eventsToBePlayed,i.paramName,i.paramValue,i.ignoreseekspeed, i.paramIsGlobal);
-                    
                                 break;
-
                         }
                     }
                 }
             }
-
-            #endregion
-
-#region Public Functions
-
 #endregion
-
-#region Private Functions
-
-#endregion
+            
         }
     }
         
